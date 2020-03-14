51ce3e1e3fc0675b566be4eba1f3b5753ba7722e
billychou <zsc1528@gmail.com>
2020-03-14 23:09:25 +0800

    init run

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
-
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func main() {
-	fmt.Printf("Hello World")
+	r := gin.New()
+
+	// Global middleware
+	// Logger middleware will write the logs to gin.DefaultWriter even if you set with GIN_MODE=release.
+	// By default gin.DefaultWriter = os.Stdout
+	r.Use(gin.Logger())
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.GET("/checkpreload.htm", func(c *gin.Context) {
+		c.String(http.StatusOK, "success")
+	})
+
+	r.GET("/test", func(c *gin.Context){
+		max := 1000000
+		sum := 0
+		for i:=0; i <= max; i++ {
+			sum += i
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"sum": sum,
+		})
+	})
+
+	r.Run()
 }
